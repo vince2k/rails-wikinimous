827c23a6ed9d16f7611c4358b8dd1610e6b53891
Vincent de Cabissole <vince2k@protonmail.com>
2025-02-07 18:03:40 +0100

    works

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new", as: :new_article
+  get "articles/:id", to: "articles#show", as: :article
+  post "articles", to: "articles#create"
+  get "articles/:id/edit", to: "articles#edit", as: :edit_article
+  patch "articles/:id", to: "articles#update"
+  delete "articles/:id", to: "articles#destroy"
+  # resources :articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,30 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+def generate_article
+  paragraphs = []
+
+  # Premier paragraphe
+  paragraphs << Faker::Lorem.sentence(word_count: 5)
+
+  # Deuxième bloc avec titre HTML
+  paragraphs << "<h3>#{Faker::Marketing.buzzwords.capitalize}</h3>"
+  3.times { paragraphs << Faker::Lorem.sentence(word_count: 5) }
+
+  # Troisième bloc avec sous-titre HTML
+  paragraphs << "<h3>#{Faker::Marketing.buzzwords.capitalize}</h3>"
+  4.times { paragraphs << Faker::Lorem.sentence(word_count: 7) }
+
+  # Quatrième bloc avec titre HTML
+  paragraphs << "<h3>#{Faker::Company.buzzword.capitalize}</h3>"
+  5.times { paragraphs << Faker::Lorem.sentence(word_count: 6)}
+
+  paragraphs.join("\n")
+end
+
+# Supprime les anciens articles pour éviter les doublons
+Article.destroy_all
+
+10.times do
+  Article.create(
+    title: Faker::Marketing.buzzwords.capitalize,
+    content: generate_article
+  )
+end
